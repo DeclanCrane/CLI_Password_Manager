cafe6cd13a84da920e75eb43e38828ea61ec8629
Declan Crane <DeclanCrane10@gmail.com>
2023-09-28 16:10:12 -0300

    Made the account listing pretty

--- utils/listAccounts.js
+++ utils/listAccounts.js
@@ -1,14 +1,18 @@
+import figlet from "figlet"
+import chalk from "chalk"
+
 export default function listAccounts(db, serviceName) {
-    if(!serviceName) {
-        for(var service in db.accounts) {
-            console.log(service)
+
+    if (!serviceName) {
+        for (var service in db.accounts) {
             db.accounts[`${service}`].map((account, idx) => {
-                console.log(`${idx}. ${account.username}, ${account.password}`)
+                console.log(chalk.blueBright(figlet.textSync((`${service}`))))
+                console.log(`${idx + 1}) User: ${chalk.greenBright(account.username)}, Pass: ${chalk.redBright(account.password)}`)
             })
         }
     } else {
         db.accounts[serviceName].map((account, idx) => {
-            console.log(`${idx}. ${account.username}, ${account.password}`)
+            console.log(`${idx + 1}) User: ${account.username}, Pass: ${account.password}`)
         })
     }
 }

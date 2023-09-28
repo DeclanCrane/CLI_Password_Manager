8922965a36816e57148be794236d18122192fb37
Declan Crane <DeclanCrane10@gmail.com>
2023-09-28 15:17:50 -0300

    Added menu

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {existsSync} from "node:fs"
+import { existsSync } from "node:fs"
 import setConfig from "./utils/setConfig.js"
 import getConfigDir from "./utils/getConfigDir.js"
 import setup from "./utils/setup.js"
@@ -6,40 +6,68 @@ import authenticate from "./utils/authenticate.js"
 import addAccount from "./utils/addAccount.js"
 import deleteAccount from "./utils/deleteAccount.js"
 import listAccounts from "./utils/listAccounts.js"
+import chalk from "chalk"
+import figlet from "figlet"
 import "dotenv/config"
+import inquirer from "inquirer"
+
+console.log(chalk.greenBright(figlet.textSync("WELCOME")));
 
 // Check if a database file exists ( setup complete )
 if (!existsSync(getConfigDir())) {
     await setup()
 }
 
-console.log("Welcome");
-
-// Get config
-//const config = getConfig();
-
 // Authenticate
-
 const config = await authenticate();
 listAccounts(config);
 
-console.log(config);
+while (true) {
+    const menu = await inquirer.prompt({
+        name: "menu",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "List Accounts",
+            "Add Account",
+            "Delete Account",
+            "Modify Account",
+            "Exit",
+        ]
+    });
 
-//await deleteAccount(config, "steam");
+    switch (menu.menu) {
+        case "List Accounts":
+            listAccounts(config);
+            break;
+        case "Add Account":
+            await addAccount(config);
+            break;
+        case "Delete Account":
+            await deleteAccount()
+            break;
+        case "Exit":
+            process.exit(0);
+    }
+}
 
-//console.log(JSON.stringify(config));
-
-setConfig(config, config.masterPass);
-
-// Decrypt database accounts
-/*
-
-*/
 
 
+// Save and exit
+const answer = await inquirer.prompt({
+    name: "changes",
+    type: "confirm",
+    message: "Save changes?",
+    default: "Y",
+});
 
-
-
+if (answer.changes) {
+    console.log(chalk.greenBright("Saving changes..."));
+    setConfig(config, config.masterPass);
+}
+else
+    console.log(chalk.redBright("Ignoring changes..."));
+process.exit(0);
 
 /*
 if (fs.existsSync(`${dbPath}db.txt`))
--- utils/authenticate.js
+++ utils/authenticate.js
@@ -1,29 +1,31 @@
-import * as readline from "readline/promises"
-import { stdin, stdout } from "node:process"
 import getConfig from "./getConfig.js";
 import decrypt from "./decrypt.js";
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+async function getPassword() {
+    const pass = await inquirer.prompt({
+        name: "pass",
+        type: "password",
+        message: "What is your password?\n"
+    })
+
+    return pass.pass;
+}
 
 export default async function authenticate() {
-    const rl = readline.createInterface({ input: stdin, output: stdout});
-
-    let password = "";
-    let authenticated = false;
     const maxRetry = 3;
-    let retryCount = 0;
     const config = getConfig();
 
-    while(!authenticated) {
-        password = await rl.question("Please enter your master password: ");
+    for(let i = 1; i <= maxRetry; i++) {
+        const password = await getPassword(); 
         const data = decrypt(config, password)
         if(data) {
-            rl.close();
-            console.log("Access granted");
+            console.log(chalk.greenBright("Access granted"));
             return data;
         } else {
-            retryCount = retryCount + 1;
-            if(retryCount > maxRetry)
-                process.exit()
-            console.warn(`Access denied: attempt (${retryCount}/${maxRetry})`);
+            console.log(chalk.redBright(`Access denied: attempt (${i}/${maxRetry})`));
         }
     }
+    process.exit(1);
 }
